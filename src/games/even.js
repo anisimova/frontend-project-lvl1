e816cb72259f63b880b502eb07030cac49e7b25a
Nadezhda Anisimova <nadya.anisimova@gmail.com>
2021-09-12 18:15:16 +0300

    new file location

---
+++ src/games/even.js
@@ -0,0 +1,17 @@
+import { base, randomNumber } from '../index.js';
+
+const checkingRandomNumbers = () => {
+  const taskName = 'Answer "yes" if the number is even, otherwise answer "no".';
+  const questionAnswerArray = [];
+  for (let i = 0; i < 6; i += 2) {
+    questionAnswerArray[i] = randomNumber();
+    if (questionAnswerArray[i] % 2 === 0) {
+      questionAnswerArray[i + 1] = 'yes';
+    } else {
+      questionAnswerArray[i + 1] = 'no';
+    }
+  }
+  base(taskName, questionAnswerArray);
+};
+
+export default checkingRandomNumbers;
