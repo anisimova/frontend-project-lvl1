617df2fdaa4f72710931e201196f5a414282770a
Nadezhda Anisimova <nadya.anisimova@gmail.com>
2021-09-16 00:20:02 +0300

    divide to function - progression - and game data

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,25 +1,30 @@
-import { base, randomNumber } from '../index.js';
+import startGame from '../index.js';
+import getRandomNumber from '../utilities.js';
+
+const getProgression = (firstNumber, stepProgression, lengthProgression) => {
+  let progression = '';
+  const space = ' ';
+  for (let n = 1; n <= lengthProgression; n += 1) {
+    progression += (firstNumber + stepProgression * n) + space;
+  }
+  return progression;
+};
 
 const fillProgression = () => {
   const taskName = 'What number is missing in the progression?';
-  const questionAnswerArray = [];
-  for (let i = 0; i < 6; i += 2) {
-    const firstNumber = randomNumber(10);
-    const stepProgression = randomNumber(10);
-    const indexHiddenElement = randomNumber(10);
-    const space = ' ';
+  const gameData = [];
+  for (let i = 0; i < 3; i += 1) {
+    const firstNumber = getRandomNumber(1, 10);
+    const stepProgression = getRandomNumber(1, 10);
+    const lengthProgression = 10;
+    const indexHiddenElement = getRandomNumber(1, lengthProgression);
     const hiddenElement = '..';
-    questionAnswerArray[i] = '';
-    for (let n = 1; n < 11; n += 1) {
-      if (indexHiddenElement === n) {
-        questionAnswerArray[i] += hiddenElement + space;
-      } else {
-        questionAnswerArray[i] += (firstNumber + stepProgression * n) + space;
-      }
-    }
-    questionAnswerArray[i + 1] = firstNumber + stepProgression * indexHiddenElement;
+    const progression = getProgression(firstNumber, stepProgression, lengthProgression);
+    const answer = firstNumber + stepProgression * indexHiddenElement;
+    const question = progression.replace(answer, hiddenElement);
+    gameData.push([question, answer]);
   }
-  base(taskName, questionAnswerArray);
+  startGame(taskName, gameData);
 };
 
 export default fillProgression;
